7fee05e444253fcf147afdfa09fc327d3c4fa024
André Luiz Albano <andre_barros@id.uff.br>
2024-04-08 12:55:58 -0300

    v0.2
    Added the interface
    Added functions

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
-from interface.mainw import Ui_MainWindowWaveleter 
-# Substitua "sua_pasta" pelo nome da pasta onde está o arquivo Ui_MainWindow.py
-# Que contém a tela inicial
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QMessageBox
+from interface.mainw import Ui_MainWindowWaveleter
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
+from matplotlib.figure import Figure
+from wavelets import Ricker, Butterworth
 
 class MainWin(QMainWindow, Ui_MainWindowWaveleter):
     def __init__(self):
@@ -11,11 +12,94 @@ class MainWin(QMainWindow, Ui_MainWindowWaveleter):
         self.setupUi(self)
         self.setWindowTitle("Waveleter")
         
+        self.figure = Figure()
+        self.canvas = FigureCanvasQTAgg(self.figure)
         
+        # Create a QVBoxLayout within your QWidget
+        self.plotLayout = QVBoxLayout(self.frame)
+        self.plotLayout.setContentsMargins(0, 0, 0, 0)  # Remove margins
+        self.plotLayout.setSpacing(0)  # Remove spacing
+        # Add the FigureCanvasQTAgg object to the layout
+        self.plotLayout.addWidget(self.canvas)
+    
+        # Set the layout of the parent widget
+        self.matplotlib_toolbar = NavigationToolbar2QT(self.canvas)
+        # Create a custom toolbar
+        # Create a button for each function and add it to the custom toolbar
+        self.homeButton.clicked.connect(self.matplotlib_toolbar.home)
+        self.backButton.clicked.connect(self.matplotlib_toolbar.back)
+        self.forwardButton.clicked.connect(self.matplotlib_toolbar.forward)
+        self.panButton.clicked.connect(self.matplotlib_toolbar.pan)
+        self.zoomButton.clicked.connect(self.matplotlib_toolbar.zoom)
+        self.configureSubPlots.clicked.connect(self.matplotlib_toolbar.configure_subplots)
+        self.editButton.clicked.connect(self.matplotlib_toolbar.edit_parameters)
+        self.saveButton.clicked.connect(self.matplotlib_toolbar.save_figure)
+
+        self.actionViewSideBar.triggered.connect(self.toggleSideBar)
         
-        self.show()
+        self.waveletSelector.addItems(['Ricker', 'Butterworth'])
+        
+        self.plotWavButton.clicked.connect(self.plot)
 
+    def plot(self):
+        self.figure.clear()
+        self.high_freq = self.highFreqInput.text()
+        self.low_freq = self.lowFreqInput.text()
+        self.samples = self.samplesInput.text()
+        self.dt = self.timeInput.text()
+        self.wavelet = self.waveletSelector.currentText()
+        
+        if self.wavelet == 'Ricker':
+            self.plottingRicker(
+                self.high_freq,
+                self.samples,
+                self.dt,
+                self.canvas
+            )
+        if self.wavelet == 'Butterworth':
+            if not self.high_freq or not self.low_freq:
+                QMessageBox.warning(self, 'Aviso', 'Frequência Alta e Frequência Baixa são obrigatórios')
+            else:
+                try:
+                    high_freq = float(self.high_freq)
+                    low_freq = float(self.low_freq)
+                except ValueError:
+                    QMessageBox.warning(self, 'Aviso', 'Frequência Alta e Frequência Baixa devem ser números válidos')
+                    return
 
+                self.plottingButter(
+                    high_freq,
+                    low_freq,
+                    self.samples,
+                    self.dt,
+                    self.canvas
+                )
+
+    def plottingRicker(self, high_freq, samples, time, canvas):
+        self.ricker = Ricker(
+            float(high_freq),
+            int(samples),
+            float(time),
+            canvas
+            )
+        return self.ricker.plotRicker()
+    
+    def plottingButter(self, high_freq, low_freq, samples, time, canvas):
+        self.butterworth = Butterworth(
+            high_freq,
+            low_freq,
+            int(samples),
+            float(time),
+            canvas
+        )
+        return self.butterworth.plotButter()
+    
+    def toggleSideBar(self):
+        if self.dockWidget.isVisible():
+            self.dockWidget.hide()
+        else:
+            self.dockWidget.show()
+    
 def main():
     app = QApplication(sys.argv)
     window = MainWin()
---
+++ wavelets.py
@@ -0,0 +1,78 @@
+import pywt
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
+
+plt.style.use(['ggplot'])
+
+class Ricker:
+    def __init__(self, high_freq, samples, dt, canvas):
+        self.high_freq = high_freq
+        self.samples = samples
+        self.dt = dt
+        self.canvas = canvas
+        
+    def waveletRicker(self):
+        twlet = np.arange(self.samples) * (self.dt / 1000)
+        twlet = np.concatenate((np.flipud(-twlet[1:]), twlet), axis=0)
+        wlet = (1. -2.*(np.pi**2)*(self.high_freq**2)*(twlet**2))*np.exp(-(np.pi**2)*(self.high_freq**2)*(twlet**2))
+        return twlet, wlet
+    
+    def plotRicker(self):
+        twlet, wlet = self.waveletRicker()
+
+        fft_r = abs(np.fft.rfft(wlet))
+        freqs_r = np.fft.rfftfreq(twlet.shape[0], d=4/1000)
+        fft_r = fft_r / np.max(fft_r)
+
+        ax = self.canvas.figure.add_subplot(211)
+        ax.plot(twlet, wlet)
+        ax.set_title('Ricker Wavelet')
+        ax1 = self.canvas.figure.add_subplot(212)
+        ax1.plot(freqs_r, fft_r)
+        ax1.set_title('Ricker Spectrum')
+        self.canvas.figure.set_tight_layout(True)
+        self.canvas.draw()
+        
+        
+class Butterworth:
+    def __init__(self, high_freq, low_freq, samples, dt, canvas):
+        self.high_freq = high_freq
+        self.low_freq = low_freq
+        self.samples = samples
+        self.dt = dt
+        self.canvas = canvas
+        
+    def waveletButter(self):
+        twlet = np.arange(self.samples) * (self.dt / 1000)
+        twlet = np.concatenate((np.flipud(-twlet[1:]), twlet), axis=0)
+        # Create impulse signal
+        imp = signal.unit_impulse(twlet.shape[0], 'mid')
+
+        # Apply high-pass Butterworth filter
+        fs = 1000 * (1 / self.dt)
+        b, a = signal.butter(4, self.high_freq, fs=fs)
+        response_zp = signal.filtfilt(b, a, imp)
+
+        # Apply low-pass Butterworth filter
+        low_b, low_a = signal.butter(2, self.low_freq, 'hp', fs=fs)
+        butter_wvlt = signal.filtfilt(low_b, low_a, response_zp)
+
+        return twlet, butter_wvlt
+    
+    def plotButter(self):
+        twlet, butter_wvlt = self.waveletButter()
+
+        fft_b = abs(np.fft.rfft(butter_wvlt))
+        freqs_b = np.fft.rfftfreq(twlet.shape[0], d=4/1000)
+        fft_b = fft_b / np.max(fft_b)
+
+        ax = self.canvas.figure.add_subplot(211)
+        ax.plot(twlet, butter_wvlt)
+        ax.set_title('Butterworth Wavelet')
+        ax1 = self.canvas.figure.add_subplot(212)
+        ax1.plot(freqs_b, fft_b)
+        ax1.set_title('Butterworth Spectrum')
+        self.canvas.figure.set_tight_layout(True)
+        self.canvas.draw()

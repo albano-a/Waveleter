4ff4da0344cef3123b1c030231551788c5fa43de
[Andre Albano] <110417140+albano-a@users.noreply.github.com>
2024-05-11 11:10:38 -0300

    chore: Refactor wavelets.py for improved readability and consistency

--- wavelets.py
+++ wavelets.py
@@ -5,7 +5,8 @@ from scipy import signal
 import matplotlib.pyplot as plt
 
 
-plt.style.use(['ggplot'])
+plt.style.use(["ggplot"])
+
 
 class Ricker:
     def __init__(self, high_freq, samples, dt, canvas):
@@ -13,29 +14,37 @@ class Ricker:
         self.samples = samples
         self.dt = dt
         self.canvas = canvas
-        
+
     def waveletRicker(self):
         twlet = np.arange(self.samples) * (self.dt / 1000)
         twlet = np.concatenate((np.flipud(-twlet[1:]), twlet), axis=0)
-        wlet = (1. -2.*(np.pi**2)*(self.high_freq**2)*(twlet**2))*np.exp(-(np.pi**2)*(self.high_freq**2)*(twlet**2))
+        wlet = (
+            1.0 - 2.0 * (np.pi**2) * (self.high_freq**2) * (twlet**2)
+        ) * np.exp(-(np.pi**2) * (self.high_freq**2) * (twlet**2))
         return twlet, wlet
-    
+
     def plotRicker(self):
         twlet, wlet = self.waveletRicker()
 
         fft_r = abs(np.fft.rfft(wlet))
-        freqs_r = np.fft.rfftfreq(twlet.shape[0], d=4/1000)
+        freqs_r = np.fft.rfftfreq(twlet.shape[0], d=4 / 1000)
         fft_r = fft_r / np.max(fft_r)
 
         ax = self.canvas.figure.add_subplot(211)
-        ax.plot(twlet, wlet)
-        ax.set_title('Ricker Wavelet')
+        ax.plot(twlet, wlet, color="navy")
+        ax.set_title("Ricker Wavelet", fontsize=14)
+        ax.set_xlabel("Tempo (s)")
+        ax.set_ylabel("Amplitude")
+
         ax1 = self.canvas.figure.add_subplot(212)
-        ax1.plot(freqs_r, fft_r)
-        ax1.set_title('Ricker Spectrum')
+        ax1.plot(freqs_r, fft_r, color="firebrick")
+        ax1.set_title("Ricker Spectrum", fontsize=14)
+        ax1.set_xlabel("Frequency (Hz)")
+        ax1.set_ylabel("Amplitude")
         self.canvas.figure.set_tight_layout(True)
         self.canvas.draw()
-                
+
+
 class Butterworth:
     def __init__(self, high_freq, low_freq, samples, dt, canvas):
         self.high_freq = high_freq
@@ -43,12 +52,12 @@ class Butterworth:
         self.samples = samples
         self.dt = dt
         self.canvas = canvas
-        
+
     def waveletButter(self):
         twlet = np.arange(self.samples) * (self.dt / 1000)
         twlet = np.concatenate((np.flipud(-twlet[1:]), twlet), axis=0)
         # Create impulse signal
-        imp = signal.unit_impulse(twlet.shape[0], 'mid')
+        imp = signal.unit_impulse(twlet.shape[0], "mid")
 
         # Apply high-pass Butterworth filter
         fs = 1000 * (1 / self.dt)
@@ -56,29 +65,43 @@ class Butterworth:
         response_zp = signal.filtfilt(b, a, imp)
 
         # Apply low-pass Butterworth filter
-        low_b, low_a = signal.butter(2, self.low_freq, 'hp', fs=fs)
+        low_b, low_a = signal.butter(2, self.low_freq, "hp", fs=fs)
         butter_wvlt = signal.filtfilt(low_b, low_a, response_zp)
 
         return twlet, butter_wvlt
-    
+
     def plotButter(self):
         twlet, butter_wvlt = self.waveletButter()
 
         fft_b = abs(np.fft.rfft(butter_wvlt))
-        freqs_b = np.fft.rfftfreq(twlet.shape[0], d=4/1000)
+        freqs_b = np.fft.rfftfreq(twlet.shape[0], d=4 / 1000)
         fft_b = fft_b / np.max(fft_b)
 
         ax = self.canvas.figure.add_subplot(211)
-        ax.plot(twlet, butter_wvlt)
-        ax.set_title('Butterworth Wavelet')
+        ax.plot(twlet, butter_wvlt, color="navy")
+        ax.set_xlabel("Tempo (s)")
+        ax.set_ylabel("Amplitude")
+        ax.set_title("Butterworth Wavelet", fontsize=14)
         ax1 = self.canvas.figure.add_subplot(212)
-        ax1.plot(freqs_b, fft_b)
-        ax1.set_title('Butterworth Spectrum')
+        ax1.plot(freqs_b, fft_b, color="firebrick")
+        ax1.set_title("Butterworth Spectrum", fontsize=14)
+        ax1.set_xlabel("Frequency (Hz)")
+        ax1.set_ylabel("Amplitude")
         self.canvas.figure.set_tight_layout(True)
         self.canvas.draw()
-        
+
+
 class Ormsby:
-    def __init__(self, high_freq, low_freq, high_cutoff_freq, low_cutoff_freq, samples, time, canvas):
+    def __init__(
+        self,
+        high_freq,
+        low_freq,
+        high_cutoff_freq,
+        low_cutoff_freq,
+        samples,
+        time,
+        canvas,
+    ):
         self.low_cut_freq = low_cutoff_freq
         self.low_pass_freq = low_freq
         self.high_pass_freq = high_freq
@@ -86,32 +109,48 @@ class Ormsby:
         self.samples = samples
         self.time = time
         self.canvas = canvas
-        
+
     def waveletOrmsby(self):
-        twlet = (np.arange(self.samples) * (self.time / 1000))
+        twlet = np.arange(self.samples) * (self.time / 1000)
         twlet = np.concatenate((np.flipud(-twlet[1:]), twlet), axis=0)
 
-        first = ( ((np.pi*self.high_cut_freq)**2) / (np.pi*self.high_cut_freq - np.pi*self.high_pass_freq)) * (pow(np.sinc(self.high_cut_freq*twlet),2))
-        second = ( ((np.pi*self.high_pass_freq)**2) / (np.pi*self.high_cut_freq - np.pi*self.high_pass_freq)) * (pow(np.sinc(self.high_pass_freq*twlet),2))
-        third = ( ((np.pi*self.low_pass_freq)**2) / (np.pi*self.low_pass_freq - np.pi*self.low_cut_freq)) * (pow(np.sinc(self.low_pass_freq*twlet), 2))
-        fourth = ( ((np.pi*self.low_cut_freq)**2) / (np.pi*self.low_pass_freq - np.pi*self.low_cut_freq)) * (pow(np.sinc(self.low_cut_freq*twlet), 2))
+        first = (
+            ((np.pi * self.high_cut_freq) ** 2)
+            / (np.pi * self.high_cut_freq - np.pi * self.high_pass_freq)
+        ) * (pow(np.sinc(self.high_cut_freq * twlet), 2))
+        second = (
+            ((np.pi * self.high_pass_freq) ** 2)
+            / (np.pi * self.high_cut_freq - np.pi * self.high_pass_freq)
+        ) * (pow(np.sinc(self.high_pass_freq * twlet), 2))
+        third = (
+            ((np.pi * self.low_pass_freq) ** 2)
+            / (np.pi * self.low_pass_freq - np.pi * self.low_cut_freq)
+        ) * (pow(np.sinc(self.low_pass_freq * twlet), 2))
+        fourth = (
+            ((np.pi * self.low_cut_freq) ** 2)
+            / (np.pi * self.low_pass_freq - np.pi * self.low_cut_freq)
+        ) * (pow(np.sinc(self.low_cut_freq * twlet), 2))
 
-        wvlet = ((first - second) - (third - fourth))
-        
+        wvlet = (first - second) - (third - fourth)
+
         return twlet, wvlet
-    
+
     def plotOrmsby(self):
         twlet, wvlet = self.waveletOrmsby()
 
         fft_o = abs(np.fft.rfft(wvlet))
-        freqs_o = np.fft.rfftfreq(twlet.shape[0], d=4/1000)
+        freqs_o = np.fft.rfftfreq(twlet.shape[0], d=4 / 1000)
         fft_o = fft_o / np.max(fft_o)
 
         ax = self.canvas.figure.add_subplot(211)
-        ax.plot(twlet, wvlet)
-        ax.set_title('Ormsby Wavelet')
+        ax.plot(twlet, wvlet, color="navy")
+        ax.set_title("Ormsby Wavelet", fontsize=14)
+        ax.set_xlabel("Time (s)")
+        ax.set_ylabel("Amplitude")
         ax1 = self.canvas.figure.add_subplot(212)
-        ax1.plot(freqs_o, fft_o)
-        ax1.set_title('Ormsby Spectrum')
+        ax1.plot(freqs_o, fft_o, color="firebrick")
+        ax1.set_title("Ormsby Spectrum", fontsize=14)
+        ax1.set_xlabel("Frequency (Hz)")
+        ax1.set_ylabel("Amplitude")
         self.canvas.figure.set_tight_layout(True)
         self.canvas.draw()
